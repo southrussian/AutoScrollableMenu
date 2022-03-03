2ec1ce8ea542f464dc6e6f0b1151822c3d9e8e37
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-03-03 23:40:33 +0300

    Initial Commit

---
+++ AutoscrollableMenu/AutoscrollableMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoscrollableMenuApp.swift
+//  AutoscrollableMenu
+//
+//  Created by Danil Peregorodiev on 03.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct AutoscrollableMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
